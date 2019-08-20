e34edb44ab223e8a9819858d0b88227f43f10c6f
kai xiong <legendarykx.kaixiong1@gmail.com>
2019-08-21 01:25:40 +0800

    Added shellScripts/test_navigation.sh

---
+++ src/shellScripts/test_navigation.sh
@@ -0,0 +1,7 @@
+
+#!/bin/sh
+xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot turtlebot_world.launch"  &
+sleep 5
+xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot amcl_demo.launch" &
+sleep 2
+xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot view_navigation.launch"
