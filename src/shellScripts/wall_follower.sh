1f064e168053fb0c0613ed57181d411393d525da
kai xiong <legendarykx.kaixiong1@gmail.com>
2019-08-21 01:14:13 +0800

    Added shellScripts/wall_follower.sh

---
+++ src/shellScripts/wall_follower.sh
@@ -0,0 +1,9 @@
+
+#!/bin/sh
+xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot turtlebot_world.launch"  &
+sleep 5
+xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot gmapping_demo.launch" &
+sleep 2
+xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot view_navigation.launch"
+sleep 2
+xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; rosrun wall_follower wall_follower_node" &
