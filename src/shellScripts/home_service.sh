cf88e23d6da29e3c3d1ab85041b9bc44a73154f1
kai xiong <legendarykx.kaixiong1@gmail.com>
2019-08-24 19:15:54 +0800

    Added shellScripts/home_service.sh

---
+++ src/shellScripts/home_service.sh
@@ -0,0 +1,19 @@
+
+#!/bin/sh
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot turtlebot_world.launch"  &
+sleep 5
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot amcl_demo.launch" &
+sleep 2
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot view_navigation.launch" &
+sleep 2
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; rosrun add_markers add_markers_node " &
+sleep 2
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects_node" &
+sleep 5
+xterm  -e "source /home/$USER/hsr_catkin_ws/devel/setup.bash; rostopic pub /pickup_goal pick_objects/goal \"first_x: {data: 4.6}
+first_y: {data: 4.8}
+first_w: {data: 1.0}
+second_x: {data: 4.2}
+second_y: {data: 0.0}
+second_w: {data: 1.0}
+check_pos: {data: true}\" --once "
