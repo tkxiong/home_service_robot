11324467b983688d79cc2501608d8374509ea391
kai xiong <legendarykx.kaixiong1@gmail.com>
2019-08-24 19:18:18 +0800

    Updated shellScripts/test_slam.sh

--- src/shellScripts/test_slam.sh
+++ src/shellScripts/test_slam.sh
@@ -1,9 +1,9 @@
 
 #!/bin/sh
-xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot turtlebot_world.launch"  &
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot turtlebot_world.launch"  &
 sleep 5
-xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot gmapping_demo.launch" &
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot gmapping_demo.launch" &
 sleep 2
-xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot keyboard_teleop.launch" &
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot keyboard_teleop.launch" &
 sleep 2
-xterm  -e  " source /home/kx/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot view_navigation.launch"
+xterm  -e  " source /home/$USER/hsr_catkin_ws/devel/setup.bash; roslaunch home_service_robot view_navigation.launch"
